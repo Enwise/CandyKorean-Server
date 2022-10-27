d871b54e850b9996a158db44b05c7c06bd9e57e2
hoosa <janghoosa@gmail.com>
2022-10-27 14:06:28 +0900

    feat: add route for createCourse api

--- src/controllers/courses.controller.ts
+++ src/controllers/courses.controller.ts
@@ -1,6 +1,7 @@
 import CoursesService from "../services/courses.service";
 import {NextFunction, Request, Response} from "express";
 import {Course} from "../interfaces/courses.interface";
+import {CreateCourseDto} from "../dtos/courses.dto";
 
 class CoursesController {
     public courseService = new CoursesService();
@@ -21,6 +22,17 @@ class CoursesController {
             const findOneCourseData: Course = await this.courseService.findCourseById(courseId);
 
             res.status(200).json({data: findOneCourseData, message: 'findOne'});
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    public createCourse = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            const courseData: CreateCourseDto = req.body;
+            const createCourseData: Course = await this.courseService.createCourse(courseData);
+
+            res.status(201).json({data: createCourseData, message: 'created'});
         } catch (error) {
             next(error);
         }
--- src/routes/courses.route.ts
+++ src/routes/courses.route.ts
@@ -1,6 +1,9 @@
 import {Routes} from "../interfaces/router.interfaces";
 import {Router} from "express";
 import CoursesController from "../controllers/courses.controller";
+import validationMiddleware from "../middlewares/validation.middleware";
+import {CreateLevelDto} from "../dtos/levels.dto";
+import {CreateCourseDto} from "../dtos/courses.dto";
 
 class CoursesRoute implements Routes {
     public path = '/course';
@@ -14,6 +17,7 @@ class CoursesRoute implements Routes {
     private initializeRoutes() {
         this.router.get(`${this.path}`, this.courseController.getCourses);
         this.router.get(`${this.path}/:id(\\d+)`, this.courseController.getCourseById);
+        this.router.post(`${this.path}`, validationMiddleware(CreateCourseDto), this.courseController.createCourse);
     }
 
 }
