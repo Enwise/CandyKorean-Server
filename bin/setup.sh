36f8495423e80d72269141298436059a8dd01c55
hoosa <janghoosa@gmail.com>
2022-11-17 23:02:03 +0900

    feat: Add ec2 init shell code

---
+++ bin/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#update apt
+sudo apt-get install update
+
+#install git, nginx, node
+sudo apt install -y git
+sudo apt install -y nginx
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
+sudo apt-get install -y nodejs
+node -v
+npm -v
+sudo npm install pm2 -g
