56286d4262e73f656c537d20b9ff2ac54959a652
hoosa <janghoosa@gmail.com>
2022-11-22 23:17:11 +0900

    fix: morgan newline bug

--- src/middlewares/multer.middleware.ts
+++ src/middlewares/multer.middleware.ts
@@ -1,5 +1,5 @@
 import {Request, Response} from "express";
-import morgan, { StreamOptions } from "morgan"
+import morgan, {StreamOptions} from "morgan"
 import logger from "../modules/winston.module";
 
 morgan.token("status", function (req, res) {
@@ -14,17 +14,17 @@ morgan.token("status", function (req, res) {
     return color + res.statusCode + "\x1B[0m";
 });
 
-morgan.token("request", (req:Request, res:Response) => {
+morgan.token("request", (req: Request, res: Response) => {
     return "body" + JSON.stringify(req.body);
 });
 
 const combined =
-    '\x1b[0m :remote-addr - :remote-user ":method :url :request HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'; // 기존 combined 포멧에서 timestamp만 제거
+    '\x1b[0m :remote-addr - :remote-user :method :url :request HTTP/:http-version :status :res[content-length] :referrer :user-agent'; // 기존 combined 포멧에서 timestamp만 제거
 const morganFormat = process.env.NODE_ENV !== "production" ? "dev" : combined; // NOTE: morgan 출력 형태 server.env에서 NODE_ENV 설정 production : 배포 dev : 개발
 
 const stream: StreamOptions = {
     write: (message) => {
-        logger.info(message);
+        logger.info(message.substring(0, message.lastIndexOf("\n")));
     },
 };
 
