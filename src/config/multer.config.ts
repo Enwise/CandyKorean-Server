08e7296477acd01707a2f33780e9194a024d5c2e
hoosa <janghoosa@gmail.com>
2022-12-06 02:35:07 +0900

    feat: add multer-s3 and multer storage config file

--- src/config/multer.config.ts
+++ src/config/multer.config.ts
@@ -1,6 +1,13 @@
 import multer from "multer";
-import {s3Storage} from "./s3.config";
+import {Request} from "express";
 
-export const upload = multer({
-    storage: s3Storage,
-})
+type FileNameCallback = (error: Error | null, filename: string) => void
+
+export const multerConfig = {
+    storage: multer.diskStorage({
+        destination: 'uploads/',
+        filename: function (req: Request, file: Express.Multer.File, cb: FileNameCallback) {
+            cb(null, file.originalname);
+        }
+    })
+}
--- src/config/s3.config.ts
+++ src/config/s3.config.ts
@@ -1,6 +1,7 @@
 import AWS from "aws-sdk"
 import {AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_REGION, AWS_BUCKET_NAME} from "./index"
 import multerS3 from "multer-s3"
+import multer from "multer";
 
 const s3 = new AWS.S3({
     accessKeyId: AWS_ACCESS_KEY,
@@ -8,10 +9,14 @@ const s3 = new AWS.S3({
     region: AWS_REGION
 });
 
-export const s3Storage = multerS3({
-    s3: s3,
-    bucket: AWS_BUCKET_NAME,
-    key: (req, file, cb) => {
-        cb(null, "files/" + Date.now() + "/" + file.originalname);
-    }
+
+export const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: AWS_BUCKET_NAME,
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        key: (req, file, cb) => {
+            cb(null, "files/" + Date.now() + "/" + file.originalname);
+        }
+    })
 });
