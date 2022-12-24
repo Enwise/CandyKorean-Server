ef44201c7ce3ff74944d3a2e1a4e2516088f1c29
sohyun <kshyun0215@gmail.com>
2022-12-25 07:01:45 +0900

    feat: add class thumbnail (#66)

--- src/entities/classes.entity.ts
+++ src/entities/classes.entity.ts
@@ -1,37 +1,42 @@
 import {
-    BaseEntity,
-    Column,
-    CreateDateColumn,
-    Entity, JoinColumn,
-    ManyToOne, OneToMany,
-    PrimaryGeneratedColumn,
-    UpdateDateColumn
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from "typeorm";
-import {Class} from "../interfaces/classes.interface";
-import {CourseEntity} from "./courses.entity";
-import {ContentsEntity} from "./contents.entity";
+import { Class } from "../interfaces/classes.interface";
+import { CourseEntity } from "./courses.entity";
+import { ContentsEntity } from "./contents.entity";
 
-@Entity('class')
+@Entity("class")
 export class ClassesEntity extends BaseEntity implements Class {
-    @PrimaryGeneratedColumn()
-    class_id: number
+  @PrimaryGeneratedColumn()
+  class_id: number;
 
-    @Column()
-    name: string
+  @Column()
+  name: string;
 
-    @CreateDateColumn()
-    date_created: Date
+  @CreateDateColumn()
+  date_created: Date;
 
-    @UpdateDateColumn()
-    date_updated: Date
+  @UpdateDateColumn()
+  date_updated: Date;
 
-    @Column()
-    course_id: number
+  @Column()
+  course_id: number;
 
-    @JoinColumn({name:"course_id"})
-    @ManyToOne(()=>CourseEntity, (course)=>course.classes)
-    course: CourseEntity;
+  @Column()
+  thumbnail: string;
 
-    @OneToMany(()=>ContentsEntity, (contents)=>contents.class_entity)
-    contents: ContentsEntity[];
+  @JoinColumn({ name: "course_id" })
+  @ManyToOne(() => CourseEntity, (course) => course.classes)
+  course: CourseEntity;
+
+  @OneToMany(() => ContentsEntity, (contents) => contents.class_entity)
+  contents: ContentsEntity[];
 }
